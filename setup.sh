deee518f1c1f912560794c6f085eaeb32d07000c
Suman Kumar ~BHUTUU <raj259942@gmail.com>
2022-07-18 15:05:22 +0530

    fixed some issues

--- setup.sh
+++ setup.sh
@@ -34,7 +34,10 @@ requirements() {
 requirements
 if [ ! -f $CWD/assets/nvim-config.zip ]; then
   printf "${S2}[${S1}!${S2}]${S1} Resources not found!${S4} Downloading....${R0}\n"
-  internetChek || wget -q "downloadlinkhereinnextcommit"
+  internetChek || wget -q "https://github.com/BHUTUU/nvimmod/blob/main/assets/nvim-config.zip?raw=true"
+  mv 'nvim-config.zip?raw=true' 'nvim-config.zip'
+  mkdir assets>/dev/null 2>&1
+  mv -v nvim-config.zip assets > /dev/null 2>&1
 fi
 printf "${S2}\tEnter the index number in which you can see nvim manual or nvim auto${R0}\n"
 cd $CWD/assets>/dev/null 2>&1
